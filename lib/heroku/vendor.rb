aa9d3fe66a9ec3a6fa1bc81f7ffec60f5059eabf
Blake Mizerany <blake@shake.local>
2010-01-18 17:43:42 -0800

    manifest check phase 1 complete

--- lib/heroku/vendor.rb
+++ lib/heroku/vendor.rb
@@ -5,14 +5,105 @@ module Heroku
   module Vendor
 
     class Manifest
+
+      class CheckFailure < StandardError ; end
+
       def initialize(man)
         @man = man
       end
 
       def check!
-        data = Yajl::Parser.parse(@man)
+
+        data = if @man.is_a? Hash
+          @man
+        else
+          Yajl::Parser.parse(@man)
+        end
+
+
+        desc "`api`" do
+
+          check "must exist" do
+            data.has_key?("api")
+          end
+
+          check "must be a hash" do
+            data["api"].is_a?(Hash)
+          end
+
+        end
+
+
+        desc "`plans`" do
+
+          check "must exist" do
+            data.has_key?("plans")
+          end
+
+          check "must be an array" do
+            data["plans"].is_a?(Array)
+          end
+
+          check "must contain at least one plan" do
+            data["plans"].size >= 1
+          end
+
+          data["plans"].each_with_index do |plan, n|
+
+            desc "at #{n} -", plan do |plan|
+
+              desc "`name`" do
+                check "must exist" do
+                  plan.has_key?("name")
+                end
+              end
+
+              desc "`price`" do
+
+                check "must exist" do
+                  plan.has_key?("price")
+                end
+
+                check "must be an integer" do
+                  plan["price"].to_s =~ /^\d+$/
+                end
+
+              end
+
+              desc "`price_unit`" do
+
+                check "must exist" do
+                  plan.has_key?("price_unit")
+                end
+
+                valid_price_units = %w|month dyno_hour|
+                check "must be [#{valid_price_units.join("|")}]" do
+                  valid_price_units.include?(plan["price_unit"])
+                end
+
+              end
+
+            end
+
+          end
+
+        end
+
       rescue Yajl::ParseError => boom
         errors boom.message
+      end
+
+      def desc(msg, o=nil, &blk)
+        temp, @desc = @desc, "#{@desc}#{msg} "
+        yield o
+      rescue CheckFailure => boom
+        errors boom.message
+      ensure
+        @desc = temp
+      end
+
+      def check(msg)
+        raise CheckFailure, "#{@desc}#{msg}" if !yield
       end
 
       def errors(msg=nil)
@@ -24,4 +115,5 @@ module Heroku
     end
 
   end
+
 end
--- test/manifest_test.rb
+++ test/manifest_test.rb
@@ -3,13 +3,136 @@ require 'heroku/vendor'
 
 class ManifestTest < Test::Unit::TestCase
 
-  test "invalid json gives error" do
-    man = Heroku::Vendor::Manifest.new("---")
-    man.check!
+  def assert_error(msg)
+    errors = Array(@man.errors)
+    assert_block "'#{msg}' is not contained in #{errors.inspect}" do
+      errors.include?(msg)
+    end
+  end
 
-    assert_not_nil man.errors
-    assert_equal 1, man.errors.size
-    assert_match /^lexical error/, man.errors.first
+  setup do
+    @manifest = {
+      "name" => "cloudcalc",
+
+      "api" => {
+        "host" => "localhost",
+        "port" => "7774",
+      },
+
+      "plans" => [
+        {
+          "name" => "Basic",
+          "price" => "0",
+          "price_unit" => "month"
+        }
+      ]
+    }
+  end
+
+  test "has no errors if everything is valid" do
+    @man = Heroku::Vendor::Manifest.new(@manifest)
+    @man.check!
+
+    assert_nil @man.errors
+  end
+
+  test "invalid json gives error" do
+    @man = Heroku::Vendor::Manifest.new("---")
+    @man.check!
+
+    assert_not_nil @man.errors
+    assert_equal 1, @man.errors.size
+    assert_match /^lexical error/, @man.errors.first
+  end
+
+  test "requires an api key" do
+    @manifest.delete("api")
+
+    @man = Heroku::Vendor::Manifest.new(@manifest)
+    @man.check!
+
+    assert_error "`api` must exist"
+  end
+
+  test "requires api key to be a Hash" do
+    @manifest["api"] = ""
+
+    @man = Heroku::Vendor::Manifest.new(@manifest)
+    @man.check!
+
+    assert_error "`api` must be a hash"
+  end
+
+  test "requires an plan key" do
+    @manifest.delete("plans")
+
+    @man = Heroku::Vendor::Manifest.new(@manifest)
+    @man.check!
+
+    assert_error "`plans` must exist"
+  end
+
+  test "requires plan to be a array" do
+    @manifest["plans"] = ""
+
+    @man = Heroku::Vendor::Manifest.new(@manifest)
+    @man.check!
+
+    assert_error "`plans` must be an array"
+  end
+
+  test "requires at least 1 plan" do
+    @manifest["plans"] = []
+
+    @man = Heroku::Vendor::Manifest.new(@manifest)
+    @man.check!
+
+    assert_error "`plans` must contain at least one plan"
+  end
+
+  test "each plan must have a name" do
+    @manifest["plans"][0].delete("name")
+
+    @man = Heroku::Vendor::Manifest.new(@manifest)
+    @man.check!
+
+    assert_error "`plans` at 0 - `name` must exist"
+  end
+
+  test "each plan must have a price" do
+    @manifest["plans"].first.delete("price")
+
+    @man = Heroku::Vendor::Manifest.new(@manifest)
+    @man.check!
+
+    assert_error "`plans` at 0 - `price` must exist"
+  end
+
+  test "each price must be an Integer" do
+    @manifest["plans"].first["price"] = "fiddy cent"
+
+    @man = Heroku::Vendor::Manifest.new(@manifest)
+    @man.check!
+
+    assert_error "`plans` at 0 - `price` must be an integer"
+  end
+
+  test "each plan must have a price unit" do
+    @manifest["plans"].first.delete("price_unit")
+
+    @man = Heroku::Vendor::Manifest.new(@manifest)
+    @man.check!
+
+    assert_error "`plans` at 0 - `price_unit` must exist"
+  end
+
+  test "each plans can only be month or dyno_hour" do
+    @manifest["plans"].first["price_unit"] = "first ov da munth"
+
+    @man = Heroku::Vendor::Manifest.new(@manifest)
+    @man.check!
+
+    assert_error "`plans` at 0 - `price_unit` must be [month|dyno_hour]"
   end
 
 end
