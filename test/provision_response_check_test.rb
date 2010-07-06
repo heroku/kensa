2c666a2e0a923b680e4fbed32e38f592bf2da127
Larry Marburger <larry@marburger.cc>
2010-07-05 21:20:48 -0400

    Fix test helper being loaded multiple times.

--- test/provision_response_check_test.rb
+++ test/provision_response_check_test.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + "/helper"
+require 'test/helper'
 
 class ProvisionResponseCheckTest < Test::Unit::TestCase
   include Heroku::Kensa
--- test/sso_check_test.rb
+++ test/sso_check_test.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + "/helper"
+require 'test/helper'
 
 class SsoCheckTest < Test::Unit::TestCase
   include Heroku::Kensa
