8313def38dd5d4aec1b474179d78423f441fe926
Larry Marburger <larry@marburger.cc>
2010-07-07 12:54:53 -0400

    Refactor Manifest.skeleton to use a hash.

--- lib/heroku/kensa/manifest.rb
+++ lib/heroku/kensa/manifest.rb
@@ -7,34 +7,25 @@ module Heroku
       end
 
       def self.skeleton
-        Yajl::Parser.parse(skeleton_str)
+        { 'id'    => 'myaddon',
+          'name'  => 'My Addon',
+          'plans' => [{
+            'id'          => 'basic',
+            'name'        => 'Basic',
+            'price'       => '0',
+            'price_unit'  => 'month' }],
+          'api' => {
+            'config_vars' => [ 'MYADDON_URL' ],
+            'production'  => 'https://yourapp.com/',
+            'test'        => 'http://localhost:4567/',
+            'username'    => 'heroku',
+            'password'    => generate_password(16),
+            'sso_salt'    => generate_password(16) }
+        }
       end
 
       def self.skeleton_str
-        return <<EOJSON
-{
-  "id": "myaddon",
-  "name": "My Addon",
-  "plans": [
-    {
-      "id": "basic",
-      "name": "Basic",
-      "price": "0",
-      "price_unit": "month"
-    }
-  ],
-  "api": {
-    "config_vars": [
-      "MYADDON_URL"
-    ],
-    "production": "https://yourapp.com/",
-    "test": "http://localhost:4567/",
-    "username": "heroku",
-    "password": "#{generate_password(16)}",
-    "sso_salt": "#{generate_password(16)}"
-  }
-}
-EOJSON
+        Yajl::Encoder.encode skeleton
       end
 
       PasswordChars = chars = ['a'..'z', 'A'..'Z', '0'..'9'].map { |r| r.to_a }.flatten
