e7d5e79da4f4e87dfcea702477c87e0cc41938fe
Chris Continanza <christopher.continanza@gmail.com>
2011-11-17 00:58:20 -0800

    test fix for both manifests

--- test/manifest_check_test.rb
+++ test/manifest_check_test.rb
@@ -5,81 +5,83 @@ class ManifestCheckTest < Test::Unit::TestCase
 
   def check ; ManifestCheck ; end
 
-  [:get, :post].each do |method|  
-    setup { @data = Manifest.new(:method => method).skeleton }
+  %w{get post}.each do |method|  
+    context "with sso #{method}" do 
+      setup { @data = Manifest.new(:method => method).skeleton }
 
-    test "is valid if no errors" do
-      assert_valid
-    end
-
-    test "has an id" do
-      @data.delete("id")
-      assert_invalid
-    end
-
-    test "api key exists" do
-      @data.delete("api")
-      assert_invalid
-    end
-
-    test "api is a Hash" do
-      @data["api"] = ""
-      assert_invalid
-    end
-
-    test "api has a password" do
-      @data["api"].delete("password")
-      assert_invalid
-    end
-
-    test "api contains test" do
-      @data["api"].delete("test")
-      assert_invalid
-    end
-
-    test "api contains production" do
-      @data["api"].delete("production")
-      assert_invalid
-    end
-
-    test "api contains production of https" do
-      if method == :get
-        @data["api"]["production"] = "http://foo.com"
-      else
-        @data["api"]["production"]['base_url'] = "http://foo.com"
+      test "is valid if no errors" do
+        assert_valid
       end
-      assert_invalid
-    end
 
-    test "api contains config_vars array" do
-      @data["api"]["config_vars"] = "test"
-      assert_invalid
-    end
+      test "has an id" do
+        @data.delete("id")
+        assert_invalid
+      end
 
-    test "api contains at least one config var" do
-      @data["api"]["config_vars"].clear
-      assert_invalid
-    end
+      test "api key exists" do
+        @data.delete("api")
+        assert_invalid
+      end
 
-    test "all config vars are in upper case" do
-      @data["api"]["config_vars"] << 'MYADDON_invalid_var'
-      assert_invalid
-    end
+      test "api is a Hash" do
+        @data["api"] = ""
+        assert_invalid
+      end
 
-    test "assert config var prefixes match addon id" do
-      @data["api"]["config_vars"] << 'MONGO_URL'
-      assert_invalid
-    end
+      test "api has a password" do
+        @data["api"].delete("password")
+        assert_invalid
+      end
 
-    test "replaces dashes for underscores on the config var check" do
-      @data["id"] = "MY-ADDON"
-      @data["api"]["config_vars"] = ["MY_ADDON_URL"]
-      assert_valid
-    end
+      test "api contains test" do
+        @data["api"].delete("test")
+        assert_invalid
+      end
 
-    test "username is deprecated" do
-      @data["api"]["username"] = "heroku"
-      assert_invalid
+      test "api contains production" do
+        @data["api"].delete("production")
+        assert_invalid
+      end
+
+      test "api contains production of https" do
+        if method == 'get'
+          @data["api"]["production"] = "http://foo.com"
+        else
+          @data["api"]["production"]['base_url'] = "http://foo.com"
+        end
+        assert_invalid
+      end
+
+      test "api contains config_vars array" do
+        @data["api"]["config_vars"] = "test"
+        assert_invalid
+      end
+
+      test "api contains at least one config var" do
+        @data["api"]["config_vars"].clear
+        assert_invalid
+      end
+
+      test "all config vars are in upper case" do
+        @data["api"]["config_vars"] << 'MYADDON_invalid_var'
+        assert_invalid
+      end
+
+      test "assert config var prefixes match addon id" do
+        @data["api"]["config_vars"] << 'MONGO_URL'
+        assert_invalid
+      end
+
+      test "replaces dashes for underscores on the config var check" do
+        @data["id"] = "MY-ADDON"
+        @data["api"]["config_vars"] = ["MY_ADDON_URL"]
+        assert_valid
+      end
+
+      test "username is deprecated" do
+        @data["api"]["username"] = "heroku"
+        assert_invalid
+      end
     end
   end
 end
