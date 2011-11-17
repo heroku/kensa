8e2f5094faffd62c19f0bba9586488b17dfee580
Chris Continanza <christopher.continanza@gmail.com>
2011-11-16 21:08:16 -0800

    use version in version file

--- lib/heroku/kensa.rb
+++ lib/heroku/kensa.rb
@@ -1,12 +1,7 @@
+require 'heroku/kensa/version'
 require 'heroku/kensa/http'
 require 'heroku/kensa/manifest'
 require 'heroku/kensa/check'
 require 'heroku/kensa/sso'
 require 'heroku/kensa/post_proxy'
 require 'heroku/kensa/screen'
-
-module Heroku
-  module Kensa
-    VERSION = "1.1.4"
-  end
-end
