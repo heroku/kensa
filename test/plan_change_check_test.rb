b1f53e6ad024f3ee19f22efa24771045cf50bdc8
Glenn Gillen <me@glenngillen.com>
2013-04-09 15:35:05 +0100

    Remove plan change tests for deprecated SSO GET manifest.

--- test/plan_change_check_test.rb
+++ test/plan_change_check_test.rb
@@ -4,29 +4,27 @@ class PlanChangeCheckTest < Test::Unit::TestCase
   include Heroku::Kensa
   include ProviderMock
 
-  %w{get post}.each do |method|
-    context "with sso #{method}" do
-      setup do
-        @data = Manifest.new(:method => method).skeleton.merge :id => 123, :plan => 'premium'
-        @data['api']['password'] = 'secret'
-      end
+  context "with sso post" do
+    setup do
+      @data = Manifest.new(:method => "post").skeleton.merge :id => 123, :plan => 'premium'
+      @data['api']['password'] = 'secret'
+    end
 
-      def check ; PlanChangeCheck ; end
+    def check ; PlanChangeCheck ; end
 
-      test "working plan change call" do
-        use_provider_endpoint "working"
-        assert_valid
-      end
+    test "working plan change call" do
+      use_provider_endpoint "working"
+      assert_valid
+    end
 
-      test "detects invalid status" do
-        use_provider_endpoint "invalid-status"
-        assert_invalid
-      end
+    test "detects invalid status" do
+      use_provider_endpoint "invalid-status"
+      assert_invalid
+    end
 
-      test "detects missing auth" do
-        use_provider_endpoint "invalid-missing-auth"
-        assert_invalid
-      end
+    test "detects missing auth" do
+      use_provider_endpoint "invalid-missing-auth"
+      assert_invalid
     end
   end
 end
