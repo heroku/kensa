6a44ac6be95cf0f66cab83f44b8c422e10689f96
Glenn Gillen <me@glenngillen.com>
2013-04-09 15:21:46 +0100

    Remove deprovisions tests for deprecated SSO GET manifest.

--- test/deprovision_check_test.rb
+++ test/deprovision_check_test.rb
@@ -3,36 +3,34 @@ require 'test/helper'
 class DeprovisionCheckTest < Test::Unit::TestCase
   include Heroku::Kensa
 
-  %w{get post}.each do |method|
-    context "with SSO #{method}" do
-      setup do
-        @data = Manifest.new(:method => method).skeleton.merge :id => 123
-        @responses = [
-          [200, ""],
-          [401, ""],
-        ]
+  context "with SSO post" do
+    setup do
+      @data = Manifest.new(:method => post).skeleton.merge :id => 123
+      @responses = [
+        [200, ""],
+        [401, ""],
+      ]
+    end
+
+    def check ; DeprovisionCheck ; end
+
+    test "valid on 200" do
+      assert_valid do |check|
+        kensa_stub :delete, check, @responses
       end
+    end
 
-      def check ; DeprovisionCheck ; end
-
-      test "valid on 200" do
-        assert_valid do |check|
-          kensa_stub :delete, check, @responses
-        end
+    test "status other than 200" do
+      @responses[0] = [500, ""]
+      assert_invalid do |check|
+        kensa_stub :delete, check, @responses
       end
+    end
 
-      test "status other than 200" do
-        @responses[0] = [500, ""]
-        assert_invalid do |check|
-          kensa_stub :delete, check, @responses
-        end
-      end
-
-      test "runs auth check" do
-        @responses[1] = [200, ""]
-        assert_invalid do |check|
-          kensa_stub :delete, check, @responses
-        end
+    test "runs auth check" do
+      @responses[1] = [200, ""]
+      assert_invalid do |check|
+        kensa_stub :delete, check, @responses
       end
     end
   end
