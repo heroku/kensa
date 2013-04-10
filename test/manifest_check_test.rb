cae2085fec1d3007e0ae23a304a3df25b7feaf2c
Glenn Gillen <me@glenngillen.com>
2013-04-10 17:10:45 +0100

    Remove uneccessary context.

--- test/manifest_check_test.rb
+++ test/manifest_check_test.rb
@@ -5,6 +5,10 @@ class ManifestCheckTest < Test::Unit::TestCase
 
   def check ; ManifestCheck ; end
 
+  setup do
+    @data = Manifest.new(:method => "post").skeleton
+  end
+
   test "doesn't barf on OkJson errors" do
     File.open("addon-manifest.json", 'w') { |f| f << "{,a" }
     assert_raises Client::CommandInvalid, "addon-manifest.json includes invalid JSON" do
@@ -12,89 +16,85 @@ class ManifestCheckTest < Test::Unit::TestCase
     end
   end
 
-  context "with sso post" do
-    setup { @data = Manifest.new(:method => "post").skeleton }
+  test "is valid if no errors" do
+    assert_valid
+  end
 
-    test "is valid if no errors" do
+  test "has an id" do
+    @data.delete("id")
+    assert_invalid
+  end
+
+  test "api key exists" do
+    @data.delete("api")
+    assert_invalid
+  end
+
+  test "api is a Hash" do
+    @data["api"] = ""
+    assert_invalid
+  end
+
+  test "api has a password" do
+    @data["api"].delete("password")
+    assert_invalid
+  end
+
+  test "api contains test" do
+    @data["api"].delete("test")
+    assert_invalid
+  end
+
+  test "api contains production" do
+    @data["api"].delete("production")
+    assert_invalid
+  end
+
+  test "api contains production of https" do
+    @data["api"]["production"]['base_url'] = "http://foo.com"
+    assert_invalid
+  end
+
+  test "sso contains production of https" do
+    @data["api"]["production"]['sso_url'] = "http://foo.com"
+    assert_invalid
+  end
+
+  test "api does not require config_vars" do
+    @data["api"].delete "config_vars"
+    assert_valid
+  end
+
+  context "with config vars" do
+    test "api contains config_vars array" do
+      @data["api"]["config_vars"] = "test"
+      assert_invalid
+    end
+
+    test "contains at least one config var" do
+      @data["api"]["config_vars"].clear
+      assert_invalid
+    end
+
+    test "all config vars are in upper case" do
+      @data["api"]["config_vars"] << 'MYADDON_invalid_var'
+      assert_invalid
+    end
+
+    test "assert config var prefixes match addon id" do
+      @data["api"]["config_vars"] << 'MONGO_URL'
+      assert_invalid
+    end
+
+    test "replaces dashes for underscores on the config var check" do
+      @data["id"] = "MY-ADDON"
+      @data["api"]["config_vars"] = ["MY_ADDON_URL"]
       assert_valid
     end
+  end
 
-    test "has an id" do
-      @data.delete("id")
-      assert_invalid
-    end
-
-    test "api key exists" do
-      @data.delete("api")
-      assert_invalid
-    end
-
-    test "api is a Hash" do
-      @data["api"] = ""
-      assert_invalid
-    end
-
-    test "api has a password" do
-      @data["api"].delete("password")
-      assert_invalid
-    end
-
-    test "api contains test" do
-      @data["api"].delete("test")
-      assert_invalid
-    end
-
-    test "api contains production" do
-      @data["api"].delete("production")
-      assert_invalid
-    end
-
-    test "api contains production of https" do
-      @data["api"]["production"]['base_url'] = "http://foo.com"
-      assert_invalid
-    end
-
-    test "sso contains production of https" do
-      @data["api"]["production"]['sso_url'] = "http://foo.com"
-      assert_invalid
-    end
-
-    test "api does not require config_vars" do
-      @data["api"].delete "config_vars"
-      assert_valid
-    end
-
-    context "with config vars" do
-      test "api contains config_vars array" do
-        @data["api"]["config_vars"] = "test"
-        assert_invalid
-      end
-
-      test "contains at least one config var" do
-        @data["api"]["config_vars"].clear
-        assert_invalid
-      end
-
-      test "all config vars are in upper case" do
-        @data["api"]["config_vars"] << 'MYADDON_invalid_var'
-        assert_invalid
-      end
-
-      test "assert config var prefixes match addon id" do
-        @data["api"]["config_vars"] << 'MONGO_URL'
-        assert_invalid
-      end
-
-      test "replaces dashes for underscores on the config var check" do
-        @data["id"] = "MY-ADDON"
-        @data["api"]["config_vars"] = ["MY_ADDON_URL"]
-        assert_valid
-      end
-    end
-
-    test "username is deprecated" do
-      @data["api"]["username"] = "heroku"
-      assert_invalid
-    end
+  test "username is deprecated" do
+    @data["api"]["username"] = "heroku"
+    assert_invalid
   end
 end
